05ac4545a7ccbdf8a27562dbf6c3bce6cfa3e829
Jaka061 <jrkny700@gmail.com>
2020-04-07 21:56:46 +0600

    exam

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.db.GenericsDb;
 
+import org.example.entity.Likes;
 import org.example.entity.Posts;
 import org.example.entity.User;
 
@@ -54,9 +55,16 @@ public class App {
         Posts posts = new Posts(user);
         Posts posts1 = new Posts(user);
         Posts posts2 = new Posts(user1);
-        Posts posts3 = new Posts(user2);
-        Posts posts4 = new Posts(user4);
-        Posts posts5 = new Posts(user3);
+        Posts posts3 = new Posts(user1);
+        Posts posts4 = new Posts(user1);
+        Posts posts5 = new Posts(user2);
+        Posts posts6 = new Posts(user2);
+        Posts posts7 = new Posts(user4);
+        Posts posts8 = new Posts(user4);
+        Posts posts9 = new Posts(user4);
+        Posts posts10 = new Posts(user3);
+        Posts posts11 = new Posts(user3);
+        Posts posts12 = new Posts(user3);
 
         postDb.create(posts);
         postDb.create(posts1);
@@ -64,10 +72,46 @@ public class App {
         postDb.create(posts3);
         postDb.create(posts4);
         postDb.create(posts5);
+        postDb.create(posts6);
+        postDb.create(posts7);
+        postDb.create(posts8);
+        postDb.create(posts9);
+        postDb.create(posts10);
+        postDb.create(posts11);
+        postDb.create(posts12);
 
+        GenericsDb<Likes> likeDb = new GenericsDb<>();
+        Likes likes = new Likes("login",posts);
+        Likes likes1 = new Likes("login1",posts1);
+        Likes likes2 = new Likes("login5",posts1);
+        Likes likes3 = new Likes("login2",posts2);
+        Likes likes4 = new Likes("login3",posts3);
+        Likes likes5 = new Likes("login4",posts4);
+        Likes likes6 = new Likes("login5",posts5);
+        Likes likes7 = new Likes("login5",posts6);
+        Likes likes8 = new Likes("login5",posts5);
+        Likes likes9 = new Likes("login5",posts7);
+        Likes likes10 = new Likes("login5",posts8);
+        Likes likes11 = new Likes("login5",posts9);
+        Likes likes12 = new Likes("login5",posts10);
+        Likes likes14 = new Likes("login5",posts11);
+        Likes likes15 = new Likes("login5",posts12);
 
-
-
+likeDb.create(likes);
+        likeDb.create(likes1);
+        likeDb.create(likes2);
+        likeDb.create(likes3);
+        likeDb.create(likes4);
+        likeDb.create(likes5);
+        likeDb.create(likes6);
+        likeDb.create(likes7);
+        likeDb.create(likes8);
+        likeDb.create(likes9);
+        likeDb.create(likes10);
+        likeDb.create(likes11);
+        likeDb.create(likes12);
+        likeDb.create(likes14);
+        likeDb.create(likes15);
 
     }
 }
